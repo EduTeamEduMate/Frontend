c600f04cca361c529592318333043f32acfc584b
Khan Sahib <saddamkhanashna786@gmail.com>
2023-11-02 20:30:56 +0200

    Main Options Menu scrollable bug fixed.

--- app/src/main/java/com/example/edumate/views/activitites/OptionsActivity.java
+++ app/src/main/java/com/example/edumate/views/activitites/OptionsActivity.java
@@ -15,7 +15,7 @@ public class OptionsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.options_activity);
 
         initRecyclerView();
         profileInit();
