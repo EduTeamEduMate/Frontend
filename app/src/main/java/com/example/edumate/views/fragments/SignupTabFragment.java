2f44b62b78f67367e5104fd99f9f8d7d5cc0fb28
@yla <@yla@DESKTOP-FOSSDE8>
2023-11-13 22:40:37 +0200

    Fixed authentication screen

--- app/src/main/java/com/example/edumate/views/fragments/SignupTabFragment.java
+++ app/src/main/java/com/example/edumate/views/fragments/SignupTabFragment.java
@@ -1,36 +1,46 @@
 package com.example.edumate.views.fragments;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.fragment.app.Fragment;
+
 import com.example.edumate.R;
 import com.example.edumate.views.activitites.EmptyPageActivity;
 
 public class SignupTabFragment extends Fragment {
 
-    EditText emailEditText, passwordEditText, confirmPasswordEditText;
+    EditText nameEditText, emailEditText, passwordEditText, confirmPasswordEditText;
 
     private OnSignupInteractionListener listener;
 
     public interface OnSignupInteractionListener {
         void onNavigateToLogin();
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_signup_tab, container, false);
 
+        nameEditText = view.findViewById(R.id.signup_name);
         emailEditText = view.findViewById(R.id.signup_email);
         passwordEditText = view.findViewById(R.id.signup_password);
         confirmPasswordEditText = view.findViewById(R.id.signup_confirm);
+
+//        final View rootView = view.findViewById(R.id.signup_root_layout);
+//        rootView.getViewTreeObserver().addOnGlobalLayoutListener(() -> {
+//            int heightDiff = rootView.getRootView().getHeight() - rootView.getHeight();
+//            if (heightDiff > 10) { // arbitrary value, adjust as needed
+//                rootView.setPadding(0, 100, 0, heightDiff);
+//            } else {
+//                rootView.setPadding(0, 100, 0, 0);
+//            }
+//        });
 
         Button signupButton = view.findViewById(R.id.signup_button);
 
@@ -38,27 +48,25 @@ public class SignupTabFragment extends Fragment {
             boolean isAllFieldsChecked = CheckAllFields();
 
             if (isAllFieldsChecked) {
-                listener.onNavigateToLogin();
+                // Navigate to the empty page (EmptyPageActivity)
+                Intent intent = new Intent(getActivity(), EmptyPageActivity.class);
+                startActivity(intent);
             }
         });
 
         return view;
     }
 
-    @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        if (context instanceof OnSignupInteractionListener) {
-            listener = (OnSignupInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString() + " must implement OnSignupInteractionListener");
-        }
-    }
-
     private boolean CheckAllFields() {
+        String name = nameEditText.getText().toString().trim();
         String email = emailEditText.getText().toString().trim();
         String password = passwordEditText.getText().toString();
         String confirmPassword = confirmPasswordEditText.getText().toString();
+
+        if (name.isEmpty()) {
+            nameEditText.setError("Name cannot be empty");
+            return false;
+        }
 
         if (email.isEmpty() || !isEmailValid(email)) {
             emailEditText.setError("Invalid or empty email");
