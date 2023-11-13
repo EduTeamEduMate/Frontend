b4b1d197e4f96543578a451891fd2ea6977de432
AdamSlei <sleimanadam6@gmail.com>
2023-11-14 00:50:54 +0200

    Added Change Name Activity Activities Connected

---
+++ app/src/main/java/com/example/edumate/views/activitites/ChangeNameActivity.java
@@ -0,0 +1,46 @@
+package com.example.edumate.views.activitites;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.edumate.R;
+import com.example.edumate.views.activitites.EmptyPageActivity;
+
+public class ChangeNameActivity extends AppCompatActivity {
+
+    EditText oldNameEditText, newNameEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_name);
+
+        oldNameEditText = findViewById(R.id.old_name);
+        newNameEditText = findViewById(R.id.new_name);
+
+        Button changeButton = findViewById(R.id.change_button);
+
+        changeButton.setOnClickListener(v -> {
+            boolean isAllFieldsChecked = checkAllFields();
+
+            if (isAllFieldsChecked) {
+                Intent intent = new Intent(ChangeNameActivity.this, EmptyPageActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private boolean checkAllFields() {
+        String oldName = oldNameEditText.getText().toString().trim();
+        String newName = newNameEditText.getText().toString().trim();
+
+        if (oldName.isEmpty()) {
+            oldNameEditText.setError("Old Name cannot be empty");
+            return false;
+        }
+
+        return true;
+    }
+}
--- app/src/main/java/com/example/edumate/views/activitites/ProfileActivity.java
+++ app/src/main/java/com/example/edumate/views/activitites/ProfileActivity.java
@@ -6,9 +6,8 @@ import android.widget.LinearLayout;
 
 import com.example.edumate.R;
 import androidx.appcompat.app.AppCompatActivity;
-
-
-
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 
 public class ProfileActivity extends AppCompatActivity {
@@ -18,7 +17,16 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        LinearLayout userName = findViewById(R.id.profile_username);
+        userName.setOnClickListener(v -> startActivity(new Intent(this, ChangeNameActivity.class)));
+
+        ConstraintLayout backbtn = findViewById(R.id.profile_back_btn);
+        backbtn.setOnClickListener(v -> startActivity(new Intent(this, OptionsActivity.class)));
+
         LinearLayout changePassword = findViewById(R.id.profile_changePassword);
         changePassword.setOnClickListener(v -> startActivity(new Intent(this, ChangePasswordActivity.class)));
+
+        CardView logout = findViewById(R.id.profile_logout);
+        logout.setOnClickListener(v -> startActivity(new Intent(this, AuthenticationActivity.class)));
     }
 }
